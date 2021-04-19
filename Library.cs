990588ed95961d4b86b12636f4e0882ed7038c61
20cmStali <fakeme184@gmail.com>
2021-04-19 09:41:43 +0300

    comment

---
+++ Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Library
+    {
+        public string LName { set; get; }
+
+        public string LAddress { set; get; }
+
+        public int LYear { set; get; }
+
+        public Library() { }
+
+        public Library(string LName, string LAddress, int LYear)
+        {
+            LName = this.LName;
+            LAddress = this.LAddress;
+            LYear = this.LYear;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Library library = new Library();
+            library.LName = "Полоцкая городская";
+            library.LAddress = "ул. Пушкина, д. Колотушкина";
+            library.LYear = 1982;
+
+            Console.WriteLine($"{library.LName}  {library.LAddress}  {library.LYear}");
+            Console.ReadKey();
+        }
+    }
+}
